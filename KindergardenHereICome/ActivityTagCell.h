92fb3d0f27526fe6da1997f9d2c33f90e39774ac
Kelli Ireland <kelli.ireland@gmail.com>
2013-11-15 17:12:25 -0500

    ActivityTagCell works

---
+++ KindergardenHereICome/ActivityTagCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityTagCell.h
+//  KindergardenHereICome
+//
+//  Created by Kelli Ireland on 2013/11/15.
+//  Copyright (c) 2013 PAEYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityTagCell : UITableViewCell
+
+@end
