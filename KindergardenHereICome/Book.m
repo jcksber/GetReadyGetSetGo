fcf54d6623b7234c64bfb1b1eec7c925dc325b59
Chris Allwein <github@allwein.org>
2013-11-15 12:56:39 -0500

    Added share methods on books and activities.

--- KindergardenHereICome/Book.m
+++ KindergardenHereICome/Book.m
@@ -7,6 +7,8 @@
 //
 
 #import "Book.h"
+#import <Social/Social.h>
+#import "ApplicationState.h"
 
 @implementation Book
 
@@ -25,6 +27,82 @@
     }
     
     return self;
+}
+
+-(void)shareOnFacebookFromViewController:(UIViewController *)controller
+{
+    NSLog(@"Share Book on Facebook");
+    
+    BOOL isFacebookAvailable = [SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook];
+    
+    if (isFacebookAvailable) {
+        NSLog(@"Facebook Available");
+        SLComposeViewController *compose = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        
+        compose.completionHandler = ^(SLComposeViewControllerResult result) {
+            switch(result) {
+                    //  This means the user cancelled without sending the Tweet
+                case SLComposeViewControllerResultCancelled:
+                    NSLog(@"Facebook Canceled!");
+                    //Show them an cancel message
+                    break;
+                    //  This means the user hit 'Send'
+                case SLComposeViewControllerResultDone:
+                    NSLog(@"Facebook Success!");
+                    //Show them an update message
+                    break;
+            }
+            
+        };
+        
+        NSString *message = [NSString stringWithFormat:@"%@ and I just read \"%@\" together!\n", [ApplicationState getInstance].currentKid.name, self.title];
+        [compose setInitialText:message];
+        [compose addURL:[NSURL URLWithString:@"http://bit.ly/2V6CFi"]];
+        [compose addImage:self.picture];
+        
+        [controller presentViewController:compose animated:YES completion:nil];
+    }
+    else{
+        NSLog(@"Facebook IS NOT Available");
+    }
+}
+
+-(void)shareOnTwitterFromViewController:(UIViewController *)controller
+{
+    NSLog(@"Share Book on Twitter");
+    
+    BOOL isTwitterAvailable = [SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter];
+    
+    if (isTwitterAvailable) {
+        NSLog(@"Twitter Available");
+        SLComposeViewController *compose = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        
+        compose.completionHandler = ^(SLComposeViewControllerResult result) {
+            switch(result) {
+                    //  This means the user cancelled without sending the Tweet
+                case SLComposeViewControllerResultCancelled:
+                    NSLog(@"Twitter Canceled!");
+                    //Show them an cancel message
+                    break;
+                    //  This means the user hit 'Send'
+                case SLComposeViewControllerResultDone:
+                    NSLog(@"Twitter Success!");
+                    //Show them an update message
+                    break;
+            }
+            
+        };
+        
+        NSString *message = [NSString stringWithFormat:@"%@ and I just read \"%@\" together!\n", [ApplicationState getInstance].currentKid.name, self.title];
+        [compose setInitialText:message];
+        [compose addURL:[NSURL URLWithString:@"http://bit.ly/2V6CFi"]];
+        [compose addImage:self.picture];
+        
+        [controller presentViewController:compose animated:YES completion:nil];
+    }
+    else{
+        NSLog(@"Twitter IS NOT Available");
+    }
 }
 
 @end
