72cdd8f4f7d1deea85d1abdbe71fe5eec570b8f0
Jack Kasbeer <jkasbeer2@gmail.com>
2013-11-15 08:14:36 -0500

    Initial Commit

---
+++ KindergardenHereICome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KindergardenHereICome
+//
+//  Created by Jack Kasbeer on 11/15/13.
+//  Copyright (c) 2013 PAEYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
