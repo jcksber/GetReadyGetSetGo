f73f459a5b48e15d8e54869e8399f04c8b1d2f3a
Kelli Ireland <kelli.ireland@gmail.com>
2013-11-16 08:40:45 -0500

    Connecting ActivityList to ActivityTagList

---
+++ KindergardenHereICome/ViewControllers/ActivityLearningInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityLearningInfoViewController.h
+//  KindergardenHereICome
+//
+//  Created by Kelli Ireland on 2013/11/16.
+//  Copyright (c) 2013 PAEYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityLearningInfoViewController : UIViewController
+
+@end
---
+++ KindergardenHereICome/ViewControllers/ActivityLearningInfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  ActivityLearningInfoViewController.m
+//  KindergardenHereICome
+//
+//  Created by Kelli Ireland on 2013/11/16.
+//  Copyright (c) 2013 PAEYC. All rights reserved.
+//
+
+#import "ActivityLearningInfoViewController.h"
+
+@interface ActivityLearningInfoViewController ()
+
+@end
+
+@implementation ActivityLearningInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
