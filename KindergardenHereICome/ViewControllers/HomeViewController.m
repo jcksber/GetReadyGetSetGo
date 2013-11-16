05671ffd891f8af782dcf575a4fc35172a02a5b5
Chris Allwein <github@allwein.org>
2013-11-16 06:59:32 -0500

    Hooked up outlets in home view. Added shake gesture for random books/activities.

--- KindergardenHereICome/ViewControllers/HomeViewController.m
+++ KindergardenHereICome/ViewControllers/HomeViewController.m
@@ -10,6 +10,7 @@
 #import "ApplicationState.h"
 #import "Book.h"
 #import "Activity.h"
+#import "ActivityListManager.h"
 
 @interface HomeViewController ()
 
@@ -35,12 +36,55 @@
     self.parentViewController.navigationItem.title = @"Home";
 }
 
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion == UIEventSubtypeMotionShake) {
+        NSLog(@"Shake, baby, shake!");
+        [self loadRandomActivityAndBook];
+    }
+}
+
+-(void)loadRandomActivityAndBook
+{
+    [ApplicationState getInstance].currentActivity = [[ActivityListManager getInstance] randomActivity];
+    [ApplicationState getInstance].currentBook = [self randomBook];
+    
+    self.todaysActivity =[ApplicationState getInstance].currentActivity;
+    self.todaysBook=    [ApplicationState getInstance].currentBook ;
+    
+}
+
+- (Book *)randomBook
+{
+    return [ [ApplicationState getInstance].currentKid.books objectAtIndex:(arc4random() % [ [ApplicationState getInstance].currentKid.books count])];
+}
+
+-(void)setTodaysActivity:(Activity *)todaysActivity
+{
+    _todaysActivity = todaysActivity;
+    self.activityTitle.text = _todaysActivity.title;
+    self.activityDescription.text = _todaysActivity.summary;
+    self.activityIcon.image = _todaysActivity.icon;
+}
+
+-(void) setTodaysBook:(Book *)todaysBook
+{
+    _todaysBook = todaysBook;
+    self.bookTitle.text = _todaysBook.title;
+    self.bookAuthor.text = _todaysBook.author;
+    self.bookCoverImageView.image = _todaysBook.picture;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    
+    [self loadRandomActivityAndBook];
     //NEED A WAY TO GET A RANDOM BOOK AND ACTIVITY
     
 	// Do any additional setup after loading the view.
